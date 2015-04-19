c730fa1884de1114def359412432cd54c826e79a
richard.turton <richard.turton@mubaloo.com>
2015-04-19 20:25:36 +0100

    Swift 1.2

--- ScrollFun/AppDelegate.swift
+++ ScrollFun/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         
-        let split = window!.rootViewController as UISplitViewController
-        let scrollContainer = split.viewControllers[1] as ScrollContainerViewController
-        let listener = (split.viewControllers[0] as UINavigationController).topViewController as ScrollViewListener
+        let split = window!.rootViewController as! UISplitViewController
+        let scrollContainer = split.viewControllers[1] as! ScrollContainerViewController
+        let listener = (split.viewControllers[0] as! UINavigationController).topViewController as! ScrollViewListener
         
         scrollContainer.scrollViewListener = listener
             
